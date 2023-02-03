2e300c8f496b9ef1d073b5cc32cd9057d245d7a4
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-02-03 17:56:55 +0200

    feat: add comments

--- src/pages/PostIdPages.jsx
+++ src/pages/PostIdPages.jsx
@@ -6,13 +6,19 @@ import PostService from "../API/PostSerice";
 const PostIdPages = () => {
   const params = useParams()
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
   const [fetchPostById, isLoading, error] = useFetching(async () => {
     const response = await PostService.getById(params.id)
     setPost(response.data)
   })
+  const [fetchComments, isComLoading, comError] = useFetching(async () => {
+    const response = await PostService.getCommentsByPostId(params.id)
+    setComments(response.data)
+  })
 
   useEffect(() => {
     fetchPostById(params.id)
+    fetchComments(params.id)
   }, [])
 
   return (
@@ -22,7 +28,24 @@ const PostIdPages = () => {
       </h1>
       {isLoading
         ? <div>Loading...</div>
-        :<div>{post.id}.{post.title}</div>
+        : <div>{post.id}.{post.title}</div>
+      }
+      <h2>Comments:</h2>
+      {
+        isComLoading
+          ? <div>Loading...</div>
+          : <div>
+            {comments.map(com =>
+            <div>
+              <h5>
+                {com.email}
+              </h5>
+              <p>
+                {com.body}
+              </p>
+            </div>
+            )}
+          </div>
       }
     </div>
   );
